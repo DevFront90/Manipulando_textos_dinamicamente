794e3988ca738a782ffb2584582af197b892fc0e
Pedro Henrique Dias de Souza <rodrigojponte@hotmail.com>
2025-06-20 11:52:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 $(function() {
-   // var el = $('.box');
+    var el = $('.box');
 
-    //el.html('<div class="test">Meu elemento!</div>');
+    el.html('<div class="test">Meu elemento!</div>');
 
-    //var meuTexto = 'Olá mundo!';
+    var meuTexto = 'Olá mundo!';
 
-    //el.html("<div class=\"teste\">"+meuTexto+"</div>"); //Essa linha em jQuery substitui todo o conteúdo do elemento el por uma nova <div> com a classe teste, apagando o que havia antes.
+    el.html("<div class=\"teste\">"+meuTexto+"</div>"); //Essa linha em jQuery substitui todo o conteúdo do elemento el por uma nova <div> com a classe teste, apagando o que havia antes.
 
-   // el.html(el.html() +'<h1 class="texto">Meu texto via javascript!</h1>');
+    el.html(el.html() +'<h1 class="texto">Meu texto via javascript!</h1>');
 
-    //console.log(el.html());
+    console.log(el.html());
 
-  //  $('.box2').text("<div></div>");
-    //$('.box2').text($('.box2').text() + "Olá Mundo!");
+    $('.box2').text("<div></div>");
+    $('.box2').text($('.box2').text() + "Olá Mundo!");
 
-  //  console.log(el.text());
-    //console.log($('.box2').text());
+   console.log(el.text());
+    console.log($('.box2').text());
 
-   // $('input[type=text]').val("Olá mundo!"); // No input type text manipulamos com o val. 
+    $('input[type=text]').val("Olá mundo!"); // No input type text manipulamos com o val. 
 
-   // $('textarea').text("Olá mundo!");
+    $('textarea').text("Olá mundo!");
 
     $('input[type=button]').click(function(){
        
           var str = $('input[type=text]').val();       
        
-          //var var2 = v.split("@"); //Split separa nossa string com base no delimitador.
+          var var2 = v.split("@"); //Split separa nossa string com base no delimitador.
           
           console.log(str.substr(1,4)); // Substr recorta nossa string.
 
